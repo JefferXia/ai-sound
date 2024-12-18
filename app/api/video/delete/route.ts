a1c4483198bd21362a41cbe3111207f6cec20183
xiazhifeng <zhifeng.xia@caocaoglobal.com>
2024-12-18 23:30:49 +0800

    增加video delete

---
+++ app/api/video/delete/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { auth } from '@/app/(auth)/auth'
+import prisma from '@/lib/prisma'
+
+export async function GET(req: NextRequest) {
+  const session = await auth()
+  const userId = session?.user?.id
+  const id = req.nextUrl.searchParams.get('id')
+  
+  if (!userId || !id) {
+    return NextResponse.json(
+      { error: "Missing required parameters" },
+      { status: 400 }
+    )
+  }
+  
+    // await prisma.video.delete({
+    //   where: { id: vid },
+    // })
+
+  await prisma.video.update({
+    where: { id: id },
+    data: {
+      status: 'DELETED'
+    }
+  })
+  return Response.json({success: true})
+}
