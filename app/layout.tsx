8eed939d17bdea366027ff38a28bf7bcc3746e8b
jefferxia <xzf3201@gmail.com>
2025-09-08 16:24:59 +0800

    接入Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,6 +68,15 @@ export default async function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Microsoft Clarity */}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "t7gm22grjv");`}
+        </Script>
+
         <Script
           src="https://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"
           strategy="beforeInteractive"
